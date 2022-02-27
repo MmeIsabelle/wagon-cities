a80f0728989b1c4b9a452c2e7ed5daa8dbdc6f83
Isabelle Lavoie <isabelle@kaastor.ca>
2022-02-27 12:30:07 -0800

    first two components loading properly without redux

---
+++ src/containers/city.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class City extends Component { 
+  render() {
+    return(
+      <div>
+        {this.props.city.name}
+      </div>
+    )
+  }
+}
+export default City;
